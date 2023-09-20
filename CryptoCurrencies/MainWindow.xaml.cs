11a6edf819390d729d5e3f63a25024e41b8626b9
DESKTOP-EDQT7Q5\brati <bratiskavova@gmail.com>
2023-09-20 10:33:14 +0300

    Add project files.

---
+++ CryptoCurrencies/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CryptoCurrencies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var data = new TestData.Data();
+            ViewListOfCurrency.ItemsSource = data.someCurrency;
+        }
+        private ListView DataView()
+        {
+            var values = new TestData.Data().someCurrency;
+            var listView = new ListView();
+            foreach (var coin in values)
+            {
+                listView.Items.Add(coin);
+            }
+            return listView;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.Trim().ToLower();
+            var filteredCoins = new TestData.Data().someCurrency.
+                Where(coin => coin.Name.ToLower().Contains(searchText)|| coin.Symbol.ToLower().Contains(searchText)).
+                ToList();
+            ViewListOfCurrency.ItemsSource = filteredCoins;
+        }
+    }
+}
---
+++ CryptoCurrencies/TestData/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCurrencies.TestData
+{
+    public class Data
+    {
+        public class ModelOfData
+        {
+            public string Name { get; set; }
+            public string Symbol { get; set; }
+            public double Price { get; set; }
+            public ModelOfData(string name, string s, double price)
+            {
+                Name = name;
+                Symbol = s;
+                Price = price;
+            }
+        }
+        public List<ModelOfData> someCurrency = new List<ModelOfData>();
+        public Data()
+        {
+            someCurrency.Add(new ModelOfData("bitcoin", "BTC", 0.001));
+            someCurrency.Add(new ModelOfData("ethereum", "ETH", 404.9774667045200896));
+            someCurrency.Add(new ModelOfData("ripple", "XRP", 0.4202870472643482));
+        }
+    }
+}
